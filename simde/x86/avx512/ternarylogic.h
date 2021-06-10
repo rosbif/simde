d73f0ce5d85c6b7f89a541ee766405caa8724c8b
Christopher Moore <moore@free.fr>
2021-06-10 07:10:02 +0200

    RFC: avx512/ternarylogic: and yet another version
    This version reduces the asymmetry between a, b and c.
    It reduces some worst-case results.

--- simde/x86/avx512/ternarylogic.h
+++ simde/x86/avx512/ternarylogic.h
@@ -34,25 +34,22 @@ HEDLEY_DIAGNOSTIC_PUSH
 SIMDE_DISABLE_UNWANTED_DIAGNOSTICS
 SIMDE_BEGIN_DECLS_
 
+#define SIMDE_X_BIT_SHUFFLE(a, b7, b6, b5, b4, b3, b2, b1, b0) \
+  (((a >> b7) & 1) << 7) | \
+  (((a >> b6) & 1) << 6) | \
+  (((a >> b5) & 1) << 5) | \
+  (((a >> b4) & 1) << 4) | \
+  (((a >> b3) & 1) << 3) | \
+  (((a >> b2) & 1) << 2) | \
+  (((a >> b1) & 1) << 1) | \
+  (((a >> b0) & 1) << 0)
+
 SIMDE_FUNCTION_ATTRIBUTES
 simde__m128i
-simde_mm_ternarylogic_epi32(simde__m128i a, simde__m128i b, simde__m128i c, int imm8)
-  SIMDE_REQUIRE_CONSTANT_RANGE(imm8, 0, 255) {
+simde_x_mm_ternarylogic_epi32(simde__m128i a, simde__m128i b, simde__m128i c, int imm8) {
   simde__m128i r, t;
   const simde__m128i zero = simde_mm_setzero_si128();
   const simde__m128i ff = simde_mm_cmpeq_epi8(zero, zero);
-  const int bits_set =
-    ((imm8 >> 7) & 1) +
-    ((imm8 >> 6) & 1) +
-    ((imm8 >> 5) & 1) +
-    ((imm8 >> 4) & 1) +
-    ((imm8 >> 3) & 1) +
-    ((imm8 >> 2) & 1) +
-    ((imm8 >> 1) & 1) +
-    ((imm8 >> 0) & 1);
-  const int negate = bits_set > 4;
-
-  if (negate) imm8 = ~imm8;
 
   r = zero;
 
@@ -82,6 +79,59 @@ simde_mm_ternarylogic_epi32(simde__m128i a, simde__m128i b, simde__m128i c, int 
     if ((imm8 & 0x03) == 0x03) r = simde_mm_or_si128(r, t);
     else if (imm8 & 0x02)      r = simde_mm_or_si128(r,    simde_mm_and_si128(c, t));
     else                       r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, t));
+  }
+
+  return r;
+}
+
+SIMDE_FUNCTION_ATTRIBUTES
+simde__m128i
+simde_mm_ternarylogic_epi32(simde__m128i a, simde__m128i b, simde__m128i c, int imm8)
+  SIMDE_REQUIRE_CONSTANT_RANGE(imm8, 0, 255) {
+  simde__m128i r;
+  const simde__m128i zero = simde_mm_setzero_si128();
+  const simde__m128i ff = simde_mm_cmpeq_epi8(zero, zero);
+  const int bits_set =
+    ((imm8 >> 7) & 1) +
+    ((imm8 >> 6) & 1) +
+    ((imm8 >> 5) & 1) +
+    ((imm8 >> 4) & 1) +
+    ((imm8 >> 3) & 1) +
+    ((imm8 >> 2) & 1) +
+    ((imm8 >> 1) & 1) +
+    ((imm8 >> 0) & 1);
+  const int negate = bits_set > 4;
+
+  if (negate) imm8 = ~imm8;
+
+  const int independent_of_c =
+    ((imm8 >> 7) & (imm8 >> 6) & 1) +
+    ((imm8 >> 5) & (imm8 >> 4) & 1) +
+    ((imm8 >> 3) & (imm8 >> 2) & 1) +
+    ((imm8 >> 1) & (imm8 >> 0) & 1);
+
+  const int independent_of_b =
+    ((imm8 >> 7) & (imm8 >> 5) & 1) +
+    ((imm8 >> 6) & (imm8 >> 4) & 1) +
+    ((imm8 >> 3) & (imm8 >> 1) & 1) +
+    ((imm8 >> 2) & (imm8 >> 0) & 1);
+
+  const int independent_of_a =
+    ((imm8 >> 7) & (imm8 >> 3) & 1) +
+    ((imm8 >> 6) & (imm8 >> 2) & 1) +
+    ((imm8 >> 5) & (imm8 >> 1) & 1) +
+    ((imm8 >> 4) & (imm8 >> 0) & 1);
+
+  if ((independent_of_a > independent_of_c) && (independent_of_a > independent_of_b)) {
+    imm8 = SIMDE_X_BIT_SHUFFLE(imm8, 7, 3, 5, 1, 6, 2, 4, 0);
+    r = simde_x_mm_ternarylogic_epi32(c, b, a, imm8);
+  }
+  else if (independent_of_b > independent_of_c) {
+    imm8 = SIMDE_X_BIT_SHUFFLE(imm8, 7, 5, 6, 4, 3, 1, 2, 0);
+    r = simde_x_mm_ternarylogic_epi32(a, c, b, imm8);
+  }
+  else {
+    r = simde_x_mm_ternarylogic_epi32(a, b, c, imm8);
   }
 
   if (negate)                  r = simde_mm_xor_si128(r, ff);
