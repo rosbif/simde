64b0592bb39e7d935261f9e4fc61cd3a534bbb9f
Christopher Moore <moore@free.fr>
2021-06-11 00:36:50 +0200

    RFC: avx512/ternarylogic: a version with many logic reductions
    For the values I have tried in godbolt this gives pretty good results.

--- simde/x86/avx512/ternarylogic.h
+++ simde/x86/avx512/ternarylogic.h
@@ -34,108 +34,141 @@ HEDLEY_DIAGNOSTIC_PUSH
 SIMDE_DISABLE_UNWANTED_DIAGNOSTICS
 SIMDE_BEGIN_DECLS_
 
-#define SIMDE_X_BIT_SHUFFLE(a, b7, b6, b5, b4, b3, b2, b1, b0) \
-  (((a >> b7) & 1) << 7) | \
-  (((a >> b6) & 1) << 6) | \
-  (((a >> b5) & 1) << 5) | \
-  (((a >> b4) & 1) << 4) | \
-  (((a >> b3) & 1) << 3) | \
-  (((a >> b2) & 1) << 2) | \
-  (((a >> b1) & 1) << 1) | \
-  (((a >> b0) & 1) << 0)
-
-SIMDE_FUNCTION_ATTRIBUTES
-simde__m128i
-simde_x_mm_ternarylogic_epi32(simde__m128i a, simde__m128i b, simde__m128i c, int imm8) {
-  simde__m128i r, t;
-  const simde__m128i zero = simde_mm_setzero_si128();
-  const simde__m128i ff = simde_mm_cmpeq_epi8(zero, zero);
-
-  r = zero;
-
-  if (imm8 & 0xc0) {
-    t = simde_mm_and_si128(a, b);
-    if ((imm8 & 0xc0) == 0xc0) r = simde_mm_or_si128(r, t);
-    else if (imm8 & 0x80)      r = simde_mm_or_si128(r,    simde_mm_and_si128(c, t));
-    else                       r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, t));
-  }
-
-  if (imm8 & 0x30) {
-    t = simde_mm_andnot_si128(b, a);
-    if ((imm8 & 0x30) == 0x30) r = simde_mm_or_si128(r, t);
-    else if (imm8 & 0x20)      r = simde_mm_or_si128(r,    simde_mm_and_si128(c, t));
-    else                       r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, t));
-  }
-
-  if (imm8 & 0x0c) {
-    t = simde_mm_andnot_si128(a, b);
-    if ((imm8 & 0x0c) == 0x0c) r = simde_mm_or_si128(r, t);
-    else if (imm8 & 0x08)      r = simde_mm_or_si128(r,    simde_mm_and_si128(c, t));
-    else                       r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, t));
-  }
-
-  if (imm8 & 0x03) {
-    t = simde_mm_xor_si128(simde_mm_or_si128(a, b), ff);
-    if ((imm8 & 0x03) == 0x03) r = simde_mm_or_si128(r, t);
-    else if (imm8 & 0x02)      r = simde_mm_or_si128(r,    simde_mm_and_si128(c, t));
-    else                       r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, t));
-  }
-
-  return r;
-}
-
 SIMDE_FUNCTION_ATTRIBUTES
 simde__m128i
 simde_mm_ternarylogic_epi32(simde__m128i a, simde__m128i b, simde__m128i c, int imm8)
   SIMDE_REQUIRE_CONSTANT_RANGE(imm8, 0, 255) {
-  simde__m128i r;
+  simde__m128i r, t;
   const simde__m128i zero = simde_mm_setzero_si128();
   const simde__m128i ff = simde_mm_cmpeq_epi8(zero, zero);
-  const int bits_set =
-    ((imm8 >> 7) & 1) +
-    ((imm8 >> 6) & 1) +
-    ((imm8 >> 5) & 1) +
-    ((imm8 >> 4) & 1) +
-    ((imm8 >> 3) & 1) +
-    ((imm8 >> 2) & 1) +
-    ((imm8 >> 1) & 1) +
-    ((imm8 >> 0) & 1);
-  const int negate = bits_set > 4;
+  int to_do, mask;
 
-  if (negate) imm8 = ~imm8;
+  to_do = imm8;
 
-  const int independent_of_c =
-    ((imm8 >> 7) & (imm8 >> 6) & 1) +
-    ((imm8 >> 5) & (imm8 >> 4) & 1) +
-    ((imm8 >> 3) & (imm8 >> 2) & 1) +
-    ((imm8 >> 1) & (imm8 >> 0) & 1);
-
-  const int independent_of_b =
-    ((imm8 >> 7) & (imm8 >> 5) & 1) +
-    ((imm8 >> 6) & (imm8 >> 4) & 1) +
-    ((imm8 >> 3) & (imm8 >> 1) & 1) +
-    ((imm8 >> 2) & (imm8 >> 0) & 1);
-
-  const int independent_of_a =
-    ((imm8 >> 7) & (imm8 >> 3) & 1) +
-    ((imm8 >> 6) & (imm8 >> 2) & 1) +
-    ((imm8 >> 5) & (imm8 >> 1) & 1) +
-    ((imm8 >> 4) & (imm8 >> 0) & 1);
-
-  if ((independent_of_a > independent_of_c) && (independent_of_a > independent_of_b)) {
-    imm8 = SIMDE_X_BIT_SHUFFLE(imm8, 7, 3, 5, 1, 6, 2, 4, 0);
-    r = simde_x_mm_ternarylogic_epi32(c, b, a, imm8);
+  mask = 0xFF;
+  if ((to_do & mask) == mask) {
+    r = ff;
+    to_do &= ~mask;
   }
-  else if (independent_of_b > independent_of_c) {
-    imm8 = SIMDE_X_BIT_SHUFFLE(imm8, 7, 5, 6, 4, 3, 1, 2, 0);
-    r = simde_x_mm_ternarylogic_epi32(a, c, b, imm8);
-  }
-  else {
-    r = simde_x_mm_ternarylogic_epi32(a, b, c, imm8);
+  else r = zero;
+
+  mask = 0xF0;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = a;
+    to_do &= ~mask;
   }
 
-  if (negate)                  r = simde_mm_xor_si128(r, ff);
+  mask = 0xCC;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, b);
+    to_do &= ~mask;
+  }
 
+  mask = 0xAA;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, c);
+    to_do &= ~mask;
+  }
+
+  mask = 0x0F;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_xor_si128(a, ff));
+    to_do &= ~mask;
+  }
+
+  mask = 0x33;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_xor_si128(b, ff));
+    to_do &= ~mask;
+  }
+
+  mask = 0x55;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_xor_si128(c, ff));
+    to_do &= ~mask;
+  }
+
+  mask = 0x3C;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_xor_si128(a, b));
+    to_do &= ~mask;
+  }
+
+  mask = 0x5A;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_xor_si128(a, c));
+    to_do &= ~mask;
+  }
+
+  mask = 0x66;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_xor_si128(b, c));
+    to_do &= ~mask;
+  }
+
+  mask = 0xA0;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_and_si128(a, c));
+    to_do &= ~mask;
+  }
+
+  mask = 0x50;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, a));
+    to_do &= ~mask;
+  }
+
+  mask = 0x0A;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_andnot_si128(a, c));
+    to_do &= ~mask;
+  }
+
+  mask = 0x88;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_and_si128(b, c));
+    to_do &= ~mask;
+  }
+
+  mask = 0x44;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, b));
+    to_do &= ~mask;
+  }
+
+  mask = 0x22;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r = simde_mm_or_si128(r, simde_mm_andnot_si128(b, c));
+    to_do &= ~mask;
+  }
+
+  if (to_do & 0xc0) {
+    t = simde_mm_and_si128(a, b);
+    if ((to_do & 0xc0) == 0xc0) r = simde_mm_or_si128(r, t);
+    else if (to_do & 0x80)      r = simde_mm_or_si128(r,    simde_mm_and_si128(c, t));
+    else                        r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, t));
+  }
+
+  if (to_do & 0x30) {
+    t = simde_mm_andnot_si128(b, a);
+    if ((to_do & 0x30) == 0x30) r = simde_mm_or_si128(r, t);
+    else if (to_do & 0x20)      r = simde_mm_or_si128(r,    simde_mm_and_si128(c, t));
+    else                        r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, t));
+  }
+
+  if (to_do & 0x0c) {
+    t = simde_mm_andnot_si128(a, b);
+    if ((to_do & 0x0c) == 0x0c) r = simde_mm_or_si128(r, t);
+    else if (to_do & 0x08)      r = simde_mm_or_si128(r,    simde_mm_and_si128(c, t));
+    else                        r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, t));
+  }
+
+  if (to_do & 0x03) {
+    t = simde_mm_xor_si128(simde_mm_or_si128(a, b), ff);
+    if ((to_do & 0x03) == 0x03) r = simde_mm_or_si128(r, t);
+    else if (to_do & 0x02)      r = simde_mm_or_si128(r,    simde_mm_and_si128(c, t));
+    else                        r = simde_mm_or_si128(r, simde_mm_andnot_si128(c, t));
+  }
   return r;
 }
 #if defined(SIMDE_X86_AVX512VL_NATIVE) && defined(SIMDE_X86_AVX512F_NATIVE)
